f5ee0eebc8bb2a195abc70cf289bb2312c7f60ef
zzjjcc <zzjjcc@yoga14s.com>
2022-06-17 19:22:10 +0800

    integrate nacos config and discovery

--- usersrv/src/main/java/sca2022/usersrv/UserSrv9701.java
+++ usersrv/src/main/java/sca2022/usersrv/UserSrv9701.java
@@ -2,8 +2,10 @@ package sca2022.usersrv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserSrv9701 {
     public static void main(String[] args) {
         SpringApplication.run(UserSrv9701.class, args);
