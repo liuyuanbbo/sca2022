d0a56cec052db7dca535fb2aedca6cbe70157231
zzjjcc <zzjjcc@yoga14s.com>
2022-06-28 14:53:31 +0800

    add nacos down

--- gateway/src/main/java/sca2022/gateway/GatewaySrv8080.java
+++ gateway/src/main/java/sca2022/gateway/GatewaySrv8080.java
@@ -1,4 +1,4 @@
-package sca2022.productsrv;
+package sca2022.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProductSrv9601 {
+public class GatewaySrv8080 {
     public static void main(String[] args) {
-        SpringApplication.run(ProductSrv9601.class, args);
+        SpringApplication.run(GatewaySrv8080.class, args);
     }
 }
--- ordersrv/src/main/java/sca2022/ordersrv/OrderSrv9501.java
+++ ordersrv/src/main/java/sca2022/ordersrv/OrderSrv9501.java
@@ -2,10 +2,12 @@ package sca2022.ordersrv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderSrv9501 {
     public static void main(String[] args) {
         SpringApplication.run(OrderSrv9501.class, args);
