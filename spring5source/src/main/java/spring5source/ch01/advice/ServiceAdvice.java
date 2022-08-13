99c66e8a587c954cc76e42da54c456c28408a064
zzjjcc <zzjjcc@yoga14s.com>
2022-08-13 10:56:22 +0800

    test ssh

--- spring5source/src/main/java/spring5source/ch01/advice/ServiceAdvice.java
+++ spring5source/src/main/java/spring5source/ch01/advice/ServiceAdvice.java
@@ -7,10 +7,10 @@ public class ServiceAdvice {
     private static final Logger LOGGER = LoggerFactory.getLogger(ServiceAdvice.class);
 
     public void beforeHandler() {
-        LOGGER.info(">>>>>> 前置处理逻辑 >>>>>> ");
+        LOGGER.info(">>>>>> 前置处理逻辑11 >>>>>> ");
     }
 
     public void afterHandler() {
-        LOGGER.info("====== 后置处理逻辑 ====== ");
+        LOGGER.info("====== 后置处理逻辑22 ====== ");
     }
 }
