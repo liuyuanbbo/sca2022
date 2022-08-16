5985ba8997d3031745a4cb7b08dc6291dfbaa582
zzjjcc <zzjjcc@yoga14s.com>
2022-08-16 21:04:44 +0800

    抽出 Chapter01 工程

--- Chapter01/src/main/java/Chapter01/mapper/UserSmsCodeMapper.java
+++
@@ -1,7 +0,0 @@
-package Chapter01.mapper;
-
-import Chapter01.entity.UserSmsCode;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface UserSmsCodeMapper extends BaseMapper<UserSmsCode> {
-}
--- Chapter01/src/main/java/Chapter01/mapper/UserinfoMapper.java
+++
@@ -1,7 +0,0 @@
-package Chapter01.mapper;
-
-import Chapter01.entity.Userinfo;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface UserinfoMapper extends BaseMapper<Userinfo> {
-}
