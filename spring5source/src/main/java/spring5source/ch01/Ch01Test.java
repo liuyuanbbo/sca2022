79b097c75b8b1d02d126dadcb5f1ac3288cd678b
zzjjcc <zzjjcc@yoga14s.com>
2022-08-21 09:45:00 +0800

    spring mvc 学习3

--- spring5source/src/main/java/spring5source/ch01/Ch01Test.java
+++ spring5source/src/main/java/spring5source/ch01/Ch01Test.java
@@ -1,13 +1,17 @@
 package spring5source.ch01;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import spring5source.ch01.service.IMessageService;
 
 @Slf4j
 public class Ch01Test {
     public static void main(String[] args) {
-        GenericApplicationContext ctx = new GenericApplicationContext();
-        ctx.refresh();
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/spring-ch01-aop.xml");
+        IMessageService messageService = ctx.getBean(IMessageService.class);
+        String echo = messageService.echo("hello world");
+        log.info("{}", echo);
         log.info("容器刷新完毕");
     }
 }
