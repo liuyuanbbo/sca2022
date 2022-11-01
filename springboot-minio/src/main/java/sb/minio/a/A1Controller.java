a0859c4a572081bfb4f410da2694952f854b0be0
zzjjcc <zzjjcc@yoga14s.com>
2022-11-01 17:12:33 +0800

    测试 curl 从文件中读取 json

--- springboot-minio/src/main/java/sb/minio/a/A1Controller.java
+++ springboot-minio/src/main/java/sb/minio/a/A1Controller.java
@@ -1,8 +1,16 @@
 package sb.minio.a;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/a01")
@@ -11,6 +19,21 @@ public class A1Controller {
     @PostMapping("/01")
     public String a1_01() {
         return "a1_01 () ...";
+    }
+
+    @PostMapping("/02")
+    public String a1_02(@RequestBody Form2 form2) {
+        return ReflectionToStringBuilder.toString(form2, ToStringStyle.MULTI_LINE_STYLE);
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    static class Form2 {
+        private Integer id;
+        private String name;
+        private String pwd;
+        private List<String> hobbies;
     }
 
 }
