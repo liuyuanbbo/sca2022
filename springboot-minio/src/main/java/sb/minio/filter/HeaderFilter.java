659dd4ee64c6b166d8c4a22a2c4d77669d615740
zzjjcc <zzjjcc@yoga14s.com>
2022-11-02 09:39:26 +0800

    x1

--- springboot-minio/src/main/java/sb/minio/filter/HeaderFilter.java
+++ springboot-minio/src/main/java/sb/minio/filter/HeaderFilter.java
@@ -1,6 +1,7 @@
 package sb.minio.filter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -18,10 +19,19 @@ public class HeaderFilter extends OncePerRequestFilter {
     @Value("${head.jsId}")
     private String jsId;
 
+    @Value("${head.urls:/a/**}")
+    private String urls;
+
+    //private static final AntPathMatcher antPathMatcher = new AntPathMatcher();
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        String[] items = StringUtils.splitByWholeSeparatorPreserveAllTokens(urls, ",");
+        boolean flag = false;
+
+        String requestURI = request.getRequestURI();
         String certId = request.getHeader("certId");
         String cusName = request.getHeader("cusName");
         String name = request.getParameter("name");
