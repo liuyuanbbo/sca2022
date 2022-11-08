1a8261f2f1a2a670aa4a209cf2491ef8682c44fa
zzjjcc <zzjjcc@yoga14s.com>
2022-11-08 10:21:29 +0800

    cg

--- spring5source/src/test/java/spring5source/ch01/test/ATest.java
+++ spring5source/src/test/java/spring5source/ch01/test/ATest.java
@@ -3,6 +3,7 @@ package spring5source.ch01.test;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ATest {
@@ -11,6 +12,15 @@ public class ATest {
         val list = List.of("AA", "BB", "CC", "DD", "AA", "BB", "CC", "DD");
         String s = list.stream().filter("CC"::equals).findFirst().orElse(null);
         System.out.println(s);
+    }
 
+    @Test
+    public void t2() {
+        String[] strArr = new String[]{"2022", "11", "08"};
+        //String[] newArr = Arrays.copyOf(strArr, 4);
+        String[] newArr = new String[4];
+        System.arraycopy(strArr, 0, newArr, 1, strArr.length);
+        newArr[0] = "test";
+        System.out.println(Arrays.toString(newArr));
     }
 }
